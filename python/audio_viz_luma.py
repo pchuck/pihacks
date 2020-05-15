1ddf2d1a3af8038fa8da04b86be9227402bd3744
patrick charles <pchuck@users.noreply.github.com>
2020-05-15 10:38:41 -0600

    parameterized display args before adopting other devices

--- python/audio_viz_luma.py
+++ python/audio_viz_luma.py
@@ -8,28 +8,32 @@
 # Uses the luma libraries: luma.core, luma.led-matrix, 
 # and requires pyaudio
 #
+# notes on (alsa) audio/mic tweaks to optimize the display:
+#   amixer sset 'Input Mux' 'Mic'
+#   amixer sset 'Mic' toggle
+#   amixer sset 'Mic Boost' 1
+#
 # Copyright (C) 2020, Patrick Charles
 # Distributed under the Mozilla Public License
 # http://www.mozilla.org/NPL/MPL-1.1.txt
 #
 import sys
+import math
 from random import randrange
+import argparse
 import pyaudio
 import wave
 from luma.core.render import canvas
 
 
 # init audio
-def init_audio(resolution, sample_rate, channels, audio_index, chunk_size):
+def init_audio(resolution, sample_rate, channels, audio_id, chunk_size):
     print('initializing audio..')
     audio = pyaudio.PyAudio() # create pyaudio instantiation
     stream = audio.open(format=resolution, rate=sample_rate, channels=channels,
-                        input_device_index=audio_index, input=True,
+                        input_device_index=audio_id, input=True,
                         frames_per_buffer=chunk_size)
     return(audio, stream)
-
-def rx(draw):
-    draw.line((0, 0, 32, 8), fill='White')
 
 #
 # initialize and return the new display device handle
@@ -71,25 +75,42 @@ def init_display(device, n, block_orientation, rotate, inreverse, intensity):
     return device
 
 # render
-def render(draw, width, height, trace):
+def render(draw, color, width, height, trace):
     for c in range(width):
         # offset origin below axis (-1) so that total silence doesn't register
-        draw.line((c, -1, c, trace[c] - 1), fill='White')
+        draw.line((c, -1, c, trace[c] - 1), fill=color)
 
 # stream/capture/viz
-def capture_and_viz(v_dev, a_dev, a_stream, width, height):
+def capture_and_viz(v_dev, a_dev, a_stream,
+                    ar, # audio resolution (e.g. 16-bit)
+                    color, width, height):
     print('capturing and visualizing..')
+    # compute the number of bits in the height resolution
+    vr = math.log(height) / math.log(2) # solving 2^vr = height, for vr
+
+    # sound to video scaling
+    # the number of bits to shift an audio value to get a video height value
+    # e.g. diff between 'ar' (audio resolution) and 'vr' (height 'resolution')
+    rshift = int(ar - vr)
+    
+    # shift one less bit so that the display is more sensitive
+    # note: this may need to change based on microphone sensitivity,
+    # but seems to yield aesthetic results, so far
+    rshift -= 1
+    print("shifting audio samples by %d (2^%d audio -> %d display height).."
+          % (rshift, ar, height))
+
+    
     # max_level = 0 # track peak recorded level (for range validation)
     trace = list(range(width)) # current display trace
-    col = 0 # current display column
-    
+    col = 0 # current display column in the display trace
     # stream
     try:
         # continually read stream and append audio chunks to frame array
         while(True):
             data = a_stream.read(chunk, exception_on_overflow = False)
             val = int.from_bytes(data, "big") # extract a value
-            level = val >> 12 # scale from 65K to 16
+            level = val >> rshift # e.g. scale from 65K to 16
             # if(level > max_level): max_level = level
             if(level < height + 1):
                 trace[col] = level
@@ -97,8 +118,7 @@ def capture_and_viz(v_dev, a_dev, a_stream, width, height):
                 if(col == width):
                     col = 0
                     with canvas(v_dev) as draw:
-                        render(draw, width, height, trace)
-                        #rx(draw)
+                        render(draw, color, width, height, trace)
 
     except KeyboardInterrupt:
         # print("max: " + str(max_level))
@@ -108,23 +128,59 @@ def capture_and_viz(v_dev, a_dev, a_stream, width, height):
         a_dev.terminate()
         pass
 
-device = 'max7219'
-cascades = 4
-orientation = 90
-rotate = 0
-inreverse = True
-intensity = 1
-v_dev = init_display(device, cascades, orientation, rotate, inreverse,
-                     intensity)
 
-res = pyaudio.paInt16 # 16-bit resolution
-chans = 1 # mono
-samp_rate = 12000 # 12K rather than 44.1KHz is sufficient here
-chunk = 1 # rather than a, say, 4K buffer. capture 1 at a time
-audio_index = 2 # device index found by p.get_device_info_by_index(ii)
-(a_dev, a_stream) = init_audio(res, samp_rate, chans, audio_index, chunk)
+## argument parsing
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='audio_viz_luma arguments',
+                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-width = 32
-height = 8
-capture_and_viz(v_dev, a_dev, a_stream, width, height)
+    # hardware constants
+    parser.add_argument('--device', '-dv', type=str, default='max7219',
+        choices=['max7219', 'ssd1306', 'ili9341'],
+        help='The type of device')
+    parser.add_argument('--cascaded', '-n', type=int, default=1,
+        help='Number of cascaded LED matrices (usu. max7219)')
+    parser.add_argument('--block-orientation', '-bo', type=int, default=0,
+        choices=[0, 90, -90],
+        help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', '-rot', type=int, default=0,
+        choices=[0, 1, 2, 3],
+        help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+    parser.add_argument('--reverse-order', '-ro', type=bool, default=False,
+        help='Set to true if blocks are in reverse order')
+    parser.add_argument('--intensity', '-i', type=int, default=128,
+        help='The intensity of the LED output (from 0..255)')
+    parser.add_argument('--color', '-c', type=str, default='White',
+        help='The color of the display (if applicable)')
+    parser.add_argument('--audio_id', '-aid', type=int, default=2,
+        help='The alsa device id of the microphone or sampling device')
+    # required positional arguments
+    parser.add_argument('x', type=int, # required!
+        help='The x resolution of the display/matrix')
+    parser.add_argument('y', type=int, # required!
+        help='The y resolution of the display/matrix')
 
+    args = parser.parse_args()
+    try:
+        v_dev = init_display(args.device,
+                             args.cascaded,
+                             args.block_orientation,
+                             args.rotate,
+                             args.reverse_order,
+                             args.intensity)
+
+        ar, ares = 16, pyaudio.paInt16 # audio resolution, in bits
+        chans = 1 # mono
+        srate = 12000 # sample rate, 12K rather than 44.1KHz is sufficient here
+        chunk = 1 # rather than a, say, 4K buffer. capture 1 at a time
+        aid = args.audio_id # use p.get_device_info_by_index() to find
+        (a_dev, a_stream) = init_audio(ares, srate, chans, aid, chunk)
+
+        # process
+        capture_and_viz(v_dev, a_dev, a_stream, ar, args.color, args.x, args.y)
+
+        
+    except KeyboardInterrupt:
+        pass
+
+##
