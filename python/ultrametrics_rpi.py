569d523b4571fde7ff89d3762c3759ee2fca46ef
patrick charles <pchuck@users.noreply.github.com>
2020-05-23 10:47:25 -0600

    constants and cleanup

--- python/ultrametrics_rpi.py
+++ python/ultrametrics_rpi.py
@@ -11,6 +11,7 @@ from luma.core.render import canvas
 
 """
   Classes for controlling Raspberry Pi GPIO devices
+    System - an encapsulation of a system with methods for interrogating state.
     Buzzer - a passive buzzer component, with start() and stop()
     ActiveBuzzer - an active buzzer component, with start() and stop()
     StatusLeds - status lights controlled individually or by threshold
@@ -22,56 +23,75 @@ from luma.core.render import canvas
     PrintDisplay - adheres to BasicDisplay interface and outputs to console
     DummyDisplay - adheres to BasicDisplay interface with noop or console out
     FileDisplay - adheres to BasicDisplay interface and writes to a data file
-    System - encapsulation of system with methods for interrogating state.
+
+    Sphinx markup is used for documentation generation.
 """
 
 class ActiveBuzzer():
     """ wrapper for controlling an active buzzer
     """
     def __init__(self, pin):
+        """
+        :param pin: The pin number (in BCM) of the buzzer's input.
+        :type pin: int
+        """
         self.pin = pin
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.OUT)
 
     def start(self):
+        """ Start the buzzer. """
         GPIO.output(self.pin, GPIO.HIGH)
 
     def stop(self):
+        """ Stop the buzzer. """
         GPIO.output(self.pin, GPIO.LOW)
 
 class PassiveBuzzer():
     """ wrapper for controlling a passive buzzer
     """
-    def __init__(self, pin):
+    def __init__(self, pin, resonance=2000, magnitude=500):
+        """
+        :param pin: The pin number (in BCM) of the buzzer's input.
+        :type pin: int
+        :param resonance: The resonant frequency in Hz of the buzzer.
+        :type resonance: int
+        :param magnitude: The magnitude of the waveform to produce.
+        :type magnitude: int
+        """
         self.pin = pin
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.OUT)
         self.pwm = GPIO.PWM(pin, 1)
         self.pwm.start(0)
-        self.magnitude = 500
-        self.resonant = 2000
-        self.period = 0.001
+        self.magnitude = magnitude
+        self.resonant = resonant
 
     def start(self):
-        self.pwm.start(50)
-        for x in range(0, 361):
-            sin_val = math.sin(x * (math.pi / 180.0))
+        """ Start the buzzer. """
+        period = 0.001
+        degrees = 360
+        self.pwm.start(0)
+        for x in range(self.degrees):
+            sin_val = math.sin(x * (math.pi / self.degrees / 2))
             tone_val = self.resonant + sin_val * self.magnitude
             self.pwm.ChangeFrequency(tone_val)
             sleep(self.period)
 
     def stop(self):
+        """ Stop the buzzer. """
         self.pwm.stop()
 
 class StatusLeds():
     """ wrapper for controlling commonly used 4-led status bar
-        used to indicate four relative levels of criticality
+        used to indicate four relative levels of criticality.
+        Order the lights by 'severity', e.g. red, ylw, grn, blu.
     """
     def __init__(self, colorpins):
-        """ specify, using BCM, the pins used, in the order: red, ylw, grn, blu
-            expects a dictionary of colors and associated bcm pin numbers
         """
-        
+        :param colorpins: The pin numbers (in BCM) of the leds.
+        :type colorpins: list
+        """
         self.colorpins = colorpins
         self.colors, self.pins = colorpins.keys(), colorpins.values()
         GPIO.setmode(GPIO.BCM)
@@ -86,14 +106,23 @@ class StatusLeds():
             GPIO.output(pin, GPIO.LOW)
 
     def light(self, color):
+        """ Light the specified led.
+        :param color: The pin number (in BCM) of the led to light
+        :type color: int
+        """
         GPIO.output(self.colorpins.get(color), GPIO.HIGH)
 
-
     def light_threshold(self, v, t1, t2):
-        """ light leds based on specified thresholds
-            note: currently assumes 3 lights and 2 thresholds
+        """ Light leds based on a value compared to thresholds. 
+        Assumes 3 lights and 2 thresholds.
+
+        :param v: The value to compare to the thresholds.
+        :type v: int
+        :param t1: The lower threshold.
+        :type t1: int
+        :param t2: The upper threshold.
+        :type t2: int
         """
-        
         if(v < t1):               
             GPIO.output(self.colorpins.get('green'), GPIO.HIGH)
         elif(v >= t1 and v < t2):
@@ -102,18 +131,28 @@ class StatusLeds():
             GPIO.output(self.colorpins.get('red'), GPIO.HIGH)
 
     def clear(self):
-        """ clear all leds
-        """
+        """ Clear all leds. """
         GPIO.output(list(self.colorpins.values()), GPIO.LOW)
 
 class DHT11:
     """ dht11 temperature and humidity sensor wrapper
     """
     def __init__(self, pin):
+        """
+        :param pin: The pin number (in BCM) of the DHT data line.
+        :type pin: int
+        """
         import adafruit_dht
         self.dht = adafruit_dht.DHT11(pin)
 
     def sense_data(self):
+        """ Read the temperature and humidity from the DHT11 sensor.
+        .. note:: RuntimeError is handled internally. The DT11 read often fails.
+
+        :return: the temperature in celsius, farenheit and the humidity.
+        :rtype: (int, int, int)
+        :raises: RuntimeError: when the DHT11 read fails
+        """
         try:
             temperature_c = self.dht.temperature
             temperature_f = temperature_c * (9 / 5) + 32
@@ -128,25 +167,46 @@ class BasicDisplay:
     """ an informal interface for displaying textual data on a display device
     """
     def clear(self):
-        """clear the display"""
+        """ Clear the display. """
         pass
+    
     def display(self, message):
-        """display the message"""
+        """ Display a message.
+
+        :param message: The message to display on the device.
+        :type message: str
+        """
         pass
     def display_formatted(self, label, sformat, value):
+        """ Display a formatted message.
+
+        :param label: The label for the value to be displayed.
+        :type label: str
+        :param sformat: The formatting string for the value.
+        :type sformate: str
+        :param value: The value to display.
+        :type value: numeric
+        """
         if(value is not None):
             self.display(label + ': ' + sformat % value)
         else:
             self.display(label + ': Error')
             
     def destroy(self):
-        """clean up the device"""
+        """ Clean up the device. """
         pass
 
 class DummyDisplay(BasicDisplay):
     """ a dummy implementation of display
     """
     def display(self, message, trace=None):
+        """ Display a message.
+
+        :param message: The message to display on the device.
+        :type message: str
+        :param trace: Ignored. Dummy displays can't display graphical traces.
+        :type trace: bool
+        """
         print(message)
     
 class LCD1602Display(BasicDisplay):
@@ -161,6 +221,10 @@ class LCD1602Display(BasicDisplay):
      4 - SCL - SCL1 (5)
     """
     def __init__(self, echo=False):
+        """
+        :param echo: Whether or not to echo writes to the logger.
+        :type echo: bool
+        """
         from PCF8574 import PCF8574_GPIO
         from Adafruit_LCD1602 import Adafruit_CharLCD
 
@@ -192,15 +256,24 @@ class LCD1602Display(BasicDisplay):
         self.lcd.message('initializing...')
 
     def clear(self):
+        """ Clear the display. """
         self.lcd.clear()
         
     def display(self, message, trace=None):
+        """ Display a message.
+
+        :param message: The message to display on the device.
+        :type message: str
+        :param trace: Ignored. LCD displays can't display graphical traces.
+        :type trace: bool
+        """
         self.lcd.clear()
         self.lcd.message(message)
         if(self.echo):
             logging.info(message)
 
     def destroy(self):
+        """ Clean up the display. """
         self.mcp.output(3, 0)
         self.lcd.clear()
 
@@ -215,14 +288,35 @@ class SSD1306Display(BasicDisplay):
      3 - SDA - SDA1 (3)
      4 - SCL - SCL1 (5)
     """
-    def __init__(self, echo=False, height=32, bh=16,
+    def __init__(self, echo=False, width=128, height=32, rotate=0,
+                 trace_height=16,
                  font=None, color='White', i2c_addr=0x3c):
+        """
+        :param echo: Whether or not to echo writes to the logger.
+        :type echo: bool
+        :param width: Number of horizontal pixels (optional, defaults to 128).
+        :type width: int
+        :param height: Number of vertical pixels (optional, defaults to 32).
+        :type height: int
+        :param rotate: Integer value of 0 (default), 1, 2 or 3 only, where 0 is
+            no rotation, 1 is rotate 90° clockwise, 2 is 180° rotation and 3
+            represents 270° rotation.
+        :type rotate: int
+        :param th: Trace height. Height in pixels of the trace graph.
+        :type th: int
+        :param font: The font.
+        :type font: Font
+        :param color: Drawing color. Ignored, if the display is monochrome.
+        :type color: str
+        :param i2c_addr: The address of the device on the i2c bus.
+        :type i2c_addr: int
+        """
         from luma.core.interface.serial import i2c
         from luma.oled.device import ssd1306 as led
 
         logging.info('looking for OLED on i2c bus at %x' % i2c_addr)
         serial = i2c(port=1, address=i2c_addr)
-        self.device = led(serial, height=height)
+        self.device = led(serial, height=height, width=width, rotate=rotate)
         logging.info('OLED found')
         self.device.clear()
         self.echo = echo
@@ -230,12 +324,13 @@ class SSD1306Display(BasicDisplay):
         self.color = color
         self.x = self.device.width
         self.y = self.device.height
-        self.bh = bh # height of bar region, in pixels
+        self.trace_height = trace_height
         with canvas(self.device) as draw:
             draw.text((0, 0), 'initializing..',
                       fill=self.color, font=self.font)
 
     def clear(self):
+        """ Clear the display. """
         self.device.clear()
 
     def _graph(self, draw, trace):
@@ -248,10 +343,18 @@ class SSD1306Display(BasicDisplay):
         delta = mxx - mnx + 1
         for xp in range(len(trace)):
             draw.line((xp, self.y,
-                       xp, self.y - (trace[xp] - mnx) * self.bh / delta),
+                       xp, self.y - (trace[xp] - mnx) *
+                           self.trace_height / delta),
                        fill=self.color)
-        
+            
     def display(self, message, trace=None):
+        """ Display a message.
+
+        :param message: The message to display on the device.
+        :type message: str
+        :param trace: Whether or not to display a graphical trace.
+        :type trace: bool
+        """
         with canvas(self.device) as draw:
             draw.text((0, 0), message, fill=self.color, font=self.font)
             if(trace is not None and len(trace) > 0):
@@ -260,18 +363,29 @@ class SSD1306Display(BasicDisplay):
             logging.info(message)
 
     def destroy(self):
+        """ Clean up the display. """
         self.device.cleanup()
 
 class PrintDisplay(BasicDisplay):
     """ implementation for displaying textual data on the console
     """
     def display(self, message):
+        """ Display a message.
+
+        :param message: The message to display on the console.
+        :type message: str
+        """
         print(message)
 
 class LogDisplay(BasicDisplay):
     """ implementation for displaying textual data on the console
     """
     def display(self, message):
+        """ Display a message.
+
+        :param message: The message to display in the logs.
+        :type message: str
+        """
         logging.info(message)
 
 
@@ -279,68 +393,27 @@ class FileDisplay(BasicDisplay):
     """ implementation for displaying textual data on the console
     """
     def __init__(self, filename, echo=False):
+        """
+        :param echo: Whether or not to echo writes to the logger.
+        :type echo: bool
+        """
         self.echo = echo
         self.file = open(filename, 'a')
         
     def display(self, message):
+        """ Display a message.
+
+        :param message: The message to the file.
+        :type message: str
+        """
         t = format('%s, %s\n' % (System.get_datetime(), message))
         self.file.write(t)
         if(self.echo):
             logging.info(t)
 
     def destroy(self):
+        """ Clean up the file resources. """
         self.file.close
-
-
-# manager capable
-#
-class OutputManager(object):
-    """ class summary line
-
-    elaboration on the docstring
-
-    Methods
-    -------
-    write(text)
-        Write text to the specified targets
-    """
-
-    valid_targets = ['logger', 'lcd', 'file']
-    targets = []
-    
-    
-    def __init__(self, targets):
-        self.targets = targets
-        if(targets.contains('LCD1602')):
-           d = LCD1602()
-           (self.lcd, self.mcp) = LCD1602.get_device()
-           
-    def write(self, text):
-        """ write some text
-
-        Parameters
-        ----------
-        text : str
-            The text to output
-
-        Returns
-        -------
-        int
-            whether or not the text was successfully written
-
-        Raises
-        ------
-        Exception
-           if the manager experienced a critical error performing the write
-
-        See Also
-        --------
-        average : Weighted average
-
-        Notes
-        -----
-        The FFT is a fast implementation of the discrete Fourier transform:
-        """
 
 class System:
     """
@@ -352,18 +425,27 @@ class System:
         self.datetime_start = get_datetime()
         self.timestamp_start = get_timestamp()
 
-    # hostname
     @staticmethod
     def get_hostname():
+        """ Fetch the current hostname.
+        :return: The name of the system
+        :rtype: str
+        """
         return os.popen('hostname').read()
 
-    # ip
     def get_ip():
+        """ Fetch the current ip address.
+        :return: The ip address of the system
+        :rtype: str
+        """
         return socket.gethostbyname(socket.gethostname())
 
-    # gpu temperature
     @staticmethod
     def get_gpu_temp():
+        """ Fetch the current GPU temperature (in degrees celsius).
+        :return: The temperature of the system's gpu
+        :rtype: float
+        """
         cmd = os.popen('vcgencmd measure_temp').read()
         cmd = cmd.split('\'C')[0] # remove the celcius designator
         l = []
@@ -375,48 +457,73 @@ class System:
         gpu = l[0]
         return gpu
     
-    # CPU temperature
     @staticmethod
     def get_cpu_temp():
+        """ Fetch the current CPU temperature (in degrees celsius).
+        :return: The temperature of the system's cpu
+        :rtype: float
+        """
         tmp = open('/sys/class/thermal/thermal_zone0/temp')
         cpu = tmp.read()
         tmp.close()
-        return float(cpu) / 1000.0
+        return float(cpu) / 1000.0 # convert from thousandths to degrees
 
-    # load
     @staticmethod
     def get_load():
-        return os.getloadavg() # 1, 5, 15min load
+        """ Fetch the current system load.
+        :return: The 1, 5 and 15 minute load on the system
+        :rtype: (float, float, float)
+        """
+        return os.getloadavg()
 
-    # load1 (non-tuple version useful as a passed function)
     @staticmethod
     def get_load1():
+        """ Fetch the current system 1min load.
+        :return: The 1min load on the system
+        :rtype: (float, float, float)
+        """
         (l1, l5, l15) = os.getloadavg()
         return l1
 
-    # uptime
     @staticmethod
     def get_uptime():
+        """
+        :return: The uptime of the system in days, hours, mins and secs.
+        :rtype: (int, int, int, int)
+        """
+        hd = 24 # hours per day
+        mh, sm = 60, 60 # minutes per hour and seconds per minute
+        sh = mh * sm # seconds per hour
+        sd = sh * hd # seconds per day
         with open('/proc/uptime', 'r') as f:
             ts = float(f.readline().split()[0])
-        days = int(ts / 86400)
-        hours = int((ts - days * 86400) / 3600)
-        minutes = int((ts - days * 86400 - hours * 3600) / 60)
-        seconds = int(ts - days * 86400 - hours * 3600 - minutes * 60)
+        days = int(ts / sd)
+        hours = int((ts - days * sd) / sh)
+        minutes = int((ts - days * sd - hours * sh) / sm)
+        seconds = int(ts - days * sd - hours * sh - minutes * sm)
         return(days, hours, minutes, seconds)
 
-    # read system time and format as a string for display
     @staticmethod
     def get_time():
+        """
+        :return: A string containing the formatted system time.
+        :rtype: str
+        """
         return datetime.now().strftime('%H:%M:%S')
 
-    # generate the datetime stamp
     @staticmethod
     def get_datetime():
+        """
+        :return: The current formatted date and system time
+        :rtype: str
+        """
         return '{:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
 
-    # generate the current timestamp
     @staticmethod
     def get_timestamp():
+        """
+        :return: The timestamp for the current time.
+        :rtype: int
+        """
         return datetime.now().timestamp()
 
